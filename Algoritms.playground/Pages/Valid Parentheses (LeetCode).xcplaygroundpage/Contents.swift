91f23ec97e19c3400652521bab614b383d574927
nikitaizosimov <48530918+nikitaizosimov@users.noreply.github.com>
2022-08-01 23:21:00 +0300

    add Valid Parentheses (LeetCode)

---
+++ Algoritms.playground/Pages/Valid Parentheses (LeetCode).xcplaygroundpage/Contents.swift
@@ -0,0 +1,32 @@
+import Foundation
+import XCTest
+
+/*:
+ # Valid Parentheses - Easy
+ https://leetcode.com/problems/valid-parentheses/
+ 
+ Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+ An input string is valid if:
+
+ Open brackets must be closed by the same type of brackets.
+ Open brackets must be closed in the correct order.
+ 
+ ## Example:
+ 
+ Input: s = "()"
+ Output: true
+ 
+ Input: s = "()[]{}"
+ Output: true
+ 
+ Input: s = "(]"
+ Output: false
+*/
+
+class Solution {
+    
+    func isValid(_ s: String) -> Bool {
+        return true
+    }
+}
