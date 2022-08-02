a2aff187ba317c02752adb6c2b4a773776e461c8
nikitaizosimov <48530918+nikitaizosimov@users.noreply.github.com>
2022-08-02 23:01:53 +0300

    solve Valid Parentheses

--- Algoritms.playground/Pages/Valid Parentheses (LeetCode).xcplaygroundpage/Contents.swift
+++ Algoritms.playground/Pages/Valid Parentheses (LeetCode).xcplaygroundpage/Contents.swift
@@ -6,9 +6,8 @@ import XCTest
  https://leetcode.com/problems/valid-parentheses/
  
  Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
-
  An input string is valid if:
-
+ 
  Open brackets must be closed by the same type of brackets.
  Open brackets must be closed in the correct order.
  
@@ -22,11 +21,65 @@ import XCTest
  
  Input: s = "(]"
  Output: false
+ 
+ Input: s = "{[]}"
+ Output: true
 */
 
 class Solution {
     
     func isValid(_ s: String) -> Bool {
-        return true
+        var symbolStack = [Character]()
+        
+        for parenthes in s {
+            if parenthes == "(" ||
+               parenthes == "{" ||
+               parenthes == "[" {
+                symbolStack.append(parenthes)
+                continue
+            }
+            
+            guard let lastStackSymbol = symbolStack.last else { return false }
+            
+            switch parenthes {
+            case ")":
+                guard lastStackSymbol == "(" else { return false }
+            case "]":
+                guard lastStackSymbol == "[" else { return false }
+            case "}":
+                guard lastStackSymbol == "{" else { return false }
+            default:
+                return false
+            }
+            
+            symbolStack.popLast()
+        }
+        
+        return symbolStack.isEmpty
     }
 }
+
+final class Tests: XCTestCase {
+    
+    func test0() {
+        let value = Solution().isValid("()")
+        XCTAssertTrue(value)
+    }
+    
+    func test1() {
+        let value = Solution().isValid("()[]{}")
+        XCTAssertTrue(value)
+    }
+
+    func test2() {
+        let value = Solution().isValid("(]")
+        XCTAssertFalse(value)
+    }
+
+    func test3() {
+        let value = Solution().isValid("(]")
+        XCTAssertFalse(value)
+    }
+}
+
+Tests.defaultTestSuite.run()
